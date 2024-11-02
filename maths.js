6f2d042b68c62f91c84870d3592cd77e656c7e05
Muaaweyah Ibn Noufal K.P <muaaweyahkp@gmail.com>
2024-11-02 16:50:45 +0530

    maths using

---
+++ maths.js
@@ -0,0 +1,19 @@
+var num1 = 2;
+var num2 = 10;
+var num3 = 3;
+
+var total = num1 + num2;            
+var minus = num2 - num1;            
+var multiple = num1 * num2;         
+var divide = num2 / num1;           
+var square = num2 * num2;           
+var cube = num2 * num2 * num2;      
+
+console.log(`${num2}+${num1}=${total}`);
+console.log(`${num2}-${num1}=${minus}`);
+console.log(`${num2}*${num1}=${multiple}`);
+console.log(`${num2}/${num1}=${divide}`);
+console.log(`${num2}^${num1}=${square}`);
+console.log(`${num2}^${num3}=${cube}`);
+                 
+               
